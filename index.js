24494b57a5ae0fc8069ed0864343b3427770e831
ASUS <abhiraskar@gmail.com>
2023-07-16 15:18:29 +0530

    crud1

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { connection } = require("./db");
 const { userRouter } = require("./Routes/userRoute");
 const { noteRouter } = require("./Routes/noteRoute");
+const cors = require("cors");
 
 const app = express()
 require("dotenv").config()
+
+app.use(cors())
 
 app.use(express.json())
 app.use("/users", userRouter)
